4d825fe9b81c5f52093ffb342bcd2ffd6e4b7988
pure-soul-kk <krishnakripa34567@gmail.com>
2023-09-14 19:37:40 +0530

    sweet:init pixysos

--- pixys_sweet.mk
+++ pixys_sweet.mk
@@ -11,10 +11,15 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from sweet device
 $(call inherit-product, device/xiaomi/sweet/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common pixys stuff.
+$(call inherit-product, vendor/pixys/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_sweet
+# PixysOS Flag
+TARGET_SUPPORTS_NEXT_GEN_ASSISTANT := true
+TARGET_GAPPS_ARCH := arm64
+TARGET_INCLUDE_LIVE_WALLPAPERS := true
+
+PRODUCT_NAME := pixys_sweet
 PRODUCT_DEVICE := sweet
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 10 Pro
